24e30bd65736336ba0aaf7df66e78ed3e6d43012
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-21 00:52:54 +0430

    I finish my calculator with Node.js , I install body-parser package from npm then I start write app.post to fix POST error

--- calculator.js
+++ calculator.js
@@ -1,10 +1,43 @@
 //jshint ES version:6
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 const port = 3000;
 
 app.listen(port, function () {
     console.log("server is running on port 3000");
+});
+
+app.post("/", function (req,res) {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let operation = req.body.opt;
+    let result;
+    switch (operation) {
+        case "+":
+            result = num1 + num2;
+            res.send("The result of the calculation is : " + result);
+        break;
+
+        case "-":
+            result = num1 - num2;
+            res.send("The result of the calculation is : " + result);
+        break;
+
+        case "*":
+            result = num1 * num2;
+            res.send("The result of the calculation is : " + result);
+        break;
+
+        case "/":
+            result = num1 / num2;
+            res.send("The result of the calculation is : " + result)
+        break;
+
+    }
 });
 
 app.get('/', function (req,res) {
