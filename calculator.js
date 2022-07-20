6e6ca41ca4c4f5774bfc2d64daa145519e491f68
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-20 21:50:02 +0430

    Use express.js to send "Hello World" from the root as the response and test Nodemon & server is working or not!

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("server connected with port 3000");
 });
 
