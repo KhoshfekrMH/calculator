093384eec3db42d64126405df0061eff81f074d1
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-20 22:03:05 +0430

    update some text!

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.listen(port, function () {
-    console.log("server connected with port 3000");
+    console.log("server is running on port 3000");
 });
 
 app.get('/', function (req,res) {
